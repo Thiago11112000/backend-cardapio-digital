58111e26f9df3b10af6f1b7aeda34700aba5c523
Thiago11112000 <70289167+Thiago11112000@users.noreply.github.com>
2024-03-26 22:54:16 -0300

    Adicionando padrão DTO ao projeto

--- src/main/java/com/example/cardapio/food/Food.java
+++ src/main/java/com/example/cardapio/food/Food.java
@@ -13,10 +13,17 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
 public class Food {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
     private Long id;
     private String title;
     private String image;
     private Integer price;
 
+    public Food(FoodRequestDTO data) {
+         this.image = data.image();
+         this.price = data.price();
+         this.title = data.title();
     }
+}
